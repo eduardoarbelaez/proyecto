c13e2b3e021250ec3306cdf22b7bf473e699edd5
eduardoarbelaez <eduardoarbelaez@hotmail.com>
2025-05-15 20:44:34 -0500

    Hasta la linea 52

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ import nltk  # NLTK es una herramienta para procesar texto y analizar palabras.
 from nltk.tokenize import word_tokenize  # Se usa para dividir un texto en palabras individuales.
 from nltk.corpus import wordnet  # Nos ayuda a encontrar sinónimos de palabras.
 
-# Indicamos la ruta donde NLTK buscará los datos descargados en nuestro computador.
-nltk.data.path.append("/opt/homebrew/lib/python3.9/site-packages/nltk_data")
+# Indicamos la ruta donde NLTK buscará los datos descargados en nuestro computador. Se puede ver ejecutando nltk.download("punkt")
+# nltk.data.path.append("/opt/homebrew/lib/python3.9/site-packages/nltk_data")
+nltk.data.path.append("/Users/eduardoarbelaez/nltk_data")
 
 # Descargar las herramientas necesarias de NLTK para el analisis de palabras.abs
 
@@ -39,3 +40,12 @@ def load_movies():
 
     # Llenamos los espacios vacios con texto vacio y convertimos los datos en una lista de diccionarios
     return df.fillna('').to_dict(orient='records')
+
+# Cargamos las peliculas al iniciar la API para no leer el archivo cada vez que alguien pregunte por ellas.
+movies_list = load_movies()
+
+# Funcion para encontrar sinonimos de una palabra.
+
+def get_synonyms(word):
+    # Usamos wordnet para encontrar distintas palabras que significan lo mismo.
+    return {lemma.name().lower() for syn in wordnet.synsets(word) for lemma in syn.lemmas()}
